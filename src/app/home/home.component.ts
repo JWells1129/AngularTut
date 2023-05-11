a45f0a824265cb15cfc250a6187abff7c1c21814
JWells1129 <jonathan.wells@tylertech.com>
2023-05-11 15:28:55 -0400

    Test
    Test thing

--- src/app/home/home.component.ts
+++ src/app/home/home.component.ts
@@ -10,20 +10,8 @@ import { HousingLocation } from '../housinglocation';
     CommonModule,
     HousingLocationComponent
   ],
-  template: `
-    <section>
-      <form>
-        <input type="text" placeholder="Filter by city">
-        <button class="primary"    type="button">Search</button>
-      </form>
-    </section>
-    <section class="results">
-      <app-housing-location
-        *ngFor="let housingLocation of housingLocationList"
-        [housingLocation]="housingLocation">
-      </app-housing-location>
-    </section>
-    `,
+  template:
+  '<p>Hello!!</p>'
   styleUrls: ['./home.component.css']
 })
 export class HomeComponent {
