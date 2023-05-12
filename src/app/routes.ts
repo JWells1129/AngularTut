f4fbfadd38b5fc75ce62148be13c5445d6be479d
JWells1129 <jonathan.wells@tylertech.com>
2023-05-12 11:20:59 -0400

    So this is the update to this branch

--- src/app/routes.ts
+++ src/app/routes.ts
@@ -16,3 +16,10 @@ const routeConfig: Routes = [
 ];
 
 export default routeConfig;
+
+
+/*
+Copyright Google LLC. All Rights Reserved.
+Use of this source code is governed by an MIT-style license that
+can be found in the LICENSE file at https://angular.io/license
+*/
