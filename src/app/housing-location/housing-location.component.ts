2b4538cb5b17841b615043296d8547fd54b2d5c9
JWells1129 <jonathan.wells@tylertech.com>
2023-05-12 12:07:41 -0400

    This is the newest update that I am saving
    description

--- src/app/housing-location/housing-location.component.ts
+++ src/app/housing-location/housing-location.component.ts
@@ -12,23 +12,16 @@ import { RouterModule } from '@angular/router';
   ],
   template: `
 
-
-
-
   <section class="listing">
     <img class="listing-photo" [src]="housingLocation.photo" alt="Exterior photo of {{housingLocation.name}}">
     <h2 class="listing-heading">{{ housingLocation.name }}</h2>
     <p class="listing-location">{{ housingLocation.city}}, {{housingLocation.state }}</p>
-    <p> lets make this the new addition to the branch</p>
     <a [routerLink]="['/details', housingLocation.id]">Learn More</a>
+
+  <p>this is a new item</p>
+
   </section>
 `,
-
-
-
-
-
-
   styleUrls: ['./housing-location.component.css'],
 })
 
